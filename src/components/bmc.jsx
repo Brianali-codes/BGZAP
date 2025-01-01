9eb854d96e8657e10988ace3af81318568584c62
Brian <gamerfunky78@gmail.com>
2025-01-01 17:36:19 +0300

    add new buy me a cofee link.

--- src/components/bmc.jsx
+++ src/components/bmc.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const BuyMeACoffeeButton = () => {
     return (
         <a 
-            href="https://buymeacoffee.com/bryce_codes" 
+            href="https://ko-fi.com/brianalcodes" 
             target="_blank" 
             rel="noopener noreferrer"
         >
             <img
-                src="https://cdn.buymeacoffee.com/buttons/default-yellow.png" 
+                src="https://storage.ko-fi.com/cdn/kofi2.png?v=6" 
                 alt="Buy Me A Coffee" 
                 style={{ width: '200px', height: 'auto',borderRadius:'10px', }} 
             />
